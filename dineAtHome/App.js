6c7333eefc418febd8f21afa997dca5ea5d743a5
yuvapriya-subramaniam <yuvaps.subramaniam@gmail.com>
2025-02-17 20:11:06 +0000

    added header image,header html changes

--- dineAtHome/App.js
+++ dineAtHome/App.js
@@ -1,6 +1,6 @@
 import react from "react";
 import reactDom from "react-dom/client";
-import logo from "./images/logo4.webp";
+import logo from "./images/logo2.png";
 
 /**
  * Header
@@ -21,24 +21,23 @@ import logo from "./images/logo4.webp";
 const Header = () => {
   return (
     <div id="headerContainer" className="header">
-      <div id="logoContainer" className="header">
-        <img id="logo" src={logo} className="header-logo" />
-      </div>
-      <div id="navBarContainer" className="header">
-        <ul id="menuContainer" className="menu">
-            <li>Home</li>
-            <li>About us</li>
-            <li>Contact</li>
-            <li>Cart</li>
-        </ul>
-      </div>
+      {/* <img id="logo" src={logo} className="header" /> */}
+      <p>Dine@Home</p>
+      <ul id="menuContainer" className="header">
+        <li>Home</li>
+        <li>About us</li>
+        <li>Contact</li>
+        <li>Cart</li>
+      </ul>
     </div>
   );
 };
 const AppLayout = () => {
-  return <div className="app">
-    <Header />
-  </div>;
+  return (
+    <div className="app">
+      <Header />
+    </div>
+  );
 };
 
 const root = reactDom.createRoot(document.getElementById("root"));
